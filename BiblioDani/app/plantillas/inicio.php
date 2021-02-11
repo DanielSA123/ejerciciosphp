19b9a18037f5dccf99187729eb8f1708dc57e36a
DanielS <danielsuebrviola@gmail.com>
2021-02-11 10:32:16 +0100

    busqueda-BiblioDani

--- BiblioDani/app/plantillas/inicio.php
+++ BiblioDani/app/plantillas/inicio.php
@@ -1,24 +1,53 @@
 <?php ob_start() ?>
 
+<div id="busqueda">
+    <form action="index.php?ctl=buscarLibro" method="post">
+        <input type="text" name="titulo" value="" placeholder="Titulo">
+        <input type="text" name="autor" value="" placeholder="Autor">
+        <select name="genero">
+            <option value="">Elige un genero</option>
+            <?php
+            foreach ($generos as $value) :
+            ?>
+            <option value="<?= $value['nombre'] ?>"><?= $value['nombre'] ?></option>
+            <?php
+            endforeach;
+            ?>
+        </select>
+        <select name="idioma">
+            <option value="">Elige un idioma</option>
+            <?php
+            foreach ($idiomas as $value) :
+            ?>
+            <option value="<?= $value['idioma'] ?>"><?= $value['idioma'] ?></option>
+            <?php
+            endforeach;
+            ?>
+        </select>
+
+        <input type="submit" value="Buscar" name="buscar">
+    </form>
+</div>
+
 <div id="listaLibros">
     <?php foreach ($libros as $value) : ?>
-        <form action="index.php?ctl=verEjemplares" method="post">
-            <div>
-                <p><strong>Título:</strong>
-                    <?= $value['titulo'] ?>.</p>
-                <p><strong>Género:</strong>
-                    <?= $value['nombre'] ?>.
-                </p>
-                <p><strong>Autor:</strong>
-                    <?= $value['autor'] ?>.</p>
-                <p><strong>Idioma:</strong>
-                    <?= $value['idioma'] ?>.</p>
-                <p><strong>Resumen:</strong>
-                    <?= $value['resumen'] ?>.</p>
-                <input type="hidden" name="idLibro" value="<?= $value['idLibro'] ?>">
-                <input type="submit" name="verEjemplares" value="Reservar">
-            </div>
-        </form>
+    <form action="index.php?ctl=verEjemplares" method="post">
+        <div>
+            <p><strong>Título:</strong>
+                <?= $value['titulo'] ?>.</p>
+            <p><strong>Género:</strong>
+                <?= $value['nombre'] ?>.
+            </p>
+            <p><strong>Autor:</strong>
+                <?= $value['autor'] ?>.</p>
+            <p><strong>Idioma:</strong>
+                <?= $value['idioma'] ?>.</p>
+            <p><strong>Resumen:</strong>
+                <?= $value['resumen'] ?>.</p>
+            <input type="hidden" name="idLibro" value="<?= $value['idLibro'] ?>">
+            <input type="submit" name="verEjemplares" value="Reservar">
+        </div>
+    </form>
     <?php endforeach; ?>
 </div>
 
