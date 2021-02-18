426ab9b79f7f3c23158a92bce4bec50e5ff878eb
DanielS <danielsuebrviola@gmail.com>
2021-02-18 21:05:22 +0100

    clase2

--- BiblioDani/app/plantillas/verGenero.php
+++ BiblioDani/app/plantillas/verGenero.php
@@ -1,13 +1,16 @@
 <?php ob_start() ?>
 
 <div id="infoGenero">
-    <p><strong>Nombre del genero:</strong>
-        <?= $genero["nombre"] ?>
-    </p>
-    <p> <strong>Descripcion:</strong>
-        <?= $genero["descripcion"] ?>
-    </p>
-
+    <?php if (isset($vacio)) : ?>
+        <p><?= $vacio ?></p>
+    <?php else : ?>
+        <p><strong>Nombre del genero:</strong>
+            <?= $genero["nombre"] ?>
+        </p>
+        <p> <strong>Descripcion:</strong>
+            <?= $genero["descripcion"] ?>
+        </p>
+    <?php endif; ?>
     <p class="centrado"><a href="index.php">Volver</a></p>
 </div>
 
